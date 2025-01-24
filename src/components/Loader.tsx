84d1502620d99e394b2a13b2d5808cbb727a7c69
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2025-01-24 01:45:12 -0400

    css styles updated

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,33 +2,11 @@ interface LoaderProps {
   text?: string;
 }
 
-export const Loader = ({ text }: LoaderProps) => {
-  return (
-    <div className="flex items-center justify-center h-full">
-      <div className="flex items-center gap-2">
-        <span
-          style={{
-            width: '30px',
-            height: '30px',
-            border: '5px solid #d1d5d5',
-            borderBottomColor: '#475659',
-            borderRadius: '9999px',
-            display: 'inline-block',
-            boxSizing: 'border-box',
-            animation: 'spin 1s linear infinite',
-          }}
-        ></span>
-        {text && (
-          <>
-            <span
-              className="text-lg font-semibold text-foreground font-mono"
-              style={{ animation: 'opacity 0.8s linear infinite alternate' }}
-            >
-              {text}
-            </span>
-          </>
-        )}
-      </div>
+export const Loader = ({ text }: LoaderProps) => (
+  <div className="flex items-center justify-center h-full" aria-label="Loading">
+    <div className="flex items-center gap-2">
+      <span className="loader"></span>
+      {text && <span className="loader-text">{text}</span>}
     </div>
-  );
-};
+  </div>
+);
